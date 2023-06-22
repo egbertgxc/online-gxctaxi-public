2a531ddedfa57d774c25914e8e5f4a98aad508d7
“xegbertgxc <1415051501@qq.com>
2023-06-23 07:26:11 +0800

    测试sourceTreexfgxfgbxfgbfg

--- service-verificationcode/src/main/java/com/gxc/serviceverificationcode/controller/TestController.java
+++ service-verificationcode/src/main/java/com/gxc/serviceverificationcode/controller/TestController.java
@@ -12,6 +12,6 @@ public class TestController {
     }
     @GetMapping("/testw")
     public String Testw(){
-        return "service-verificationcodggggggggggggg";
+        return "service-verificationcodggggggggggggggggggggggggg";
     }
 }
