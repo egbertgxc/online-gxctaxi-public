689e33b2dbfb4dffada2bf6a517568ca6c8e3c10
“xegbertgxc <1415051501@qq.com>
2023-06-23 15:48:22 +0800

    server-passenger-user注册到nacos

--- service-passenger-user/src/main/java/com/gxc/servicepassengeruser/ServicePassengerUserApplication.java
+++ service-passenger-user/src/main/java/com/gxc/servicepassengeruser/ServicePassengerUserApplication.java
@@ -3,11 +3,14 @@ package com.gxc.servicepassengeruser;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @MapperScan("com.gxc.servicepassengeruser.mapper")
+@EnableDiscoveryClient
+@EnableFeignClients
 public class ServicePassengerUserApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServicePassengerUserApplication.class, args);
