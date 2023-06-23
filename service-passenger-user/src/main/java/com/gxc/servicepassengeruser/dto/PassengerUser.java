ee619f1b8b9dbd60dd7ab037a9e7e35a262fcd99
“xegbertgxc <1415051501@qq.com>
2023-06-23 15:41:46 +0800

    插入用户

--- service-passenger-user/src/main/java/com/gxc/servicepassengeruser/dto/PassengerUser.java
+++ service-passenger-user/src/main/java/com/gxc/servicepassengeruser/dto/PassengerUser.java
@@ -2,13 +2,14 @@ package com.gxc.servicepassengeruser.dto;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Data
 public class PassengerUser {
     private Long id;
-    private LocalTime gmtCreate;
-    private LocalTime gmtModified;
+    private LocalDateTime gmtCreate;
+    private LocalDateTime gmtModified;
     private String passengerPhone;
     private String passengerName;
     private byte passengerGender;
