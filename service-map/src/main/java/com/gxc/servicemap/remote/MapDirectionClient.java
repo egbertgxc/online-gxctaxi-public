fe249ff5cb18d6a0bc502fda59e04f96accccfa4
“xegbertgxc <1415051501@qq.com>
2023-06-24 22:01:41 +0800

    调用地图接口,获取路径信息

---
+++ service-map/src/main/java/com/gxc/servicemap/remote/MapDirectionClient.java
@@ -0,0 +1,77 @@
+package com.gxc.servicemap.remote;
+
+import com.gax.internalcommon.constent.AmapConfigConstans;
+import com.gax.internalcommon.dto.ResponseResult;
+import com.gax.internalcommon.responese.DirectionResponse;
+import lombok.extern.slf4j.Slf4j;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@Slf4j
+public class MapDirectionClient {
+
+    @Value("${amap.key}")
+    private String amapKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public DirectionResponse direction(String depLongitude, String depLatitude, String destLongitude, String destLatitude) {
+        //  组装请求调用url
+        /**
+         * https://restapi.amap.com/v3/direction/driving?origin=116.481028,39.989643&destination=116.465302,40.004717&extensions=all&output=json&key=7a90a823f2e7245d276028af9f4e76dd
+         */
+        StringBuilder urlBuild = new StringBuilder();
+
+        urlBuild.append(AmapConfigConstans.DIRECTION_URL);
+        urlBuild.append("?origin=" + depLongitude + "," + depLatitude + "&destination=" + destLongitude + "," + destLatitude);
+        urlBuild.append("&extensions=base&output=json");
+        urlBuild.append("&key=" + amapKey);
+        log.info(urlBuild.toString());
+
+        //  调用高德接口
+        ResponseEntity<String> directionEntity = restTemplate.getForEntity(urlBuild.toString(), String.class);
+        String directionString = directionEntity.getBody();
+        log.info("高德地图.路径规划,返回信息" + directionString);
+        //  解析接口
+
+        DirectionResponse responseResult = paresDirectionEntity(directionString);
+
+        return responseResult;
+    }
+
+    private DirectionResponse paresDirectionEntity(String directionString) {
+
+        DirectionResponse directionResponse = null;
+        try {
+            //最外层
+            JSONObject result = JSONObject.fromObject(directionString);
+            if (result.has(AmapConfigConstans.STATUS)) {
+                int status = result.getInt(AmapConfigConstans.STATUS);
+                if (status == 1 && result.has(AmapConfigConstans.ROUTE)) {
+                    JSONObject routeObject = result.getJSONObject(AmapConfigConstans.ROUTE);
+                    JSONArray pathsArray = routeObject.getJSONArray(AmapConfigConstans.PATHS);
+                    JSONObject pathObject = pathsArray.getJSONObject(0);
+                    directionResponse = new DirectionResponse();
+                    if (pathObject.has(AmapConfigConstans.DISTANCE)) {
+                        int distance = pathObject.getInt(AmapConfigConstans.DISTANCE);
+                        directionResponse.setDistance(distance);
+                    }
+                    if (pathObject.has(AmapConfigConstans.DURATION)) {
+                        int duration = pathObject.getInt(AmapConfigConstans.DURATION);
+                        directionResponse.setDuration(duration);
+                    }
+                }
+            }
+        } catch (Exception e) {
+
+        }
+        return directionResponse;
+    }
+}
--- service-map/src/main/java/com/gxc/servicemap/service/DirectionService.java
+++ service-map/src/main/java/com/gxc/servicemap/service/DirectionService.java
@@ -3,13 +3,17 @@ package com.gxc.servicemap.service;
 import com.gax.internalcommon.dto.ResponseResult;
 import com.gax.internalcommon.responese.DirectionResponse;
 import com.gax.internalcommon.responese.ForecastPriceResponse;
+import com.gxc.servicemap.remote.MapDirectionClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class DirectionService {
 
+    @Autowired
+    private MapDirectionClient mapDirectionClient;
     /**
      *
      *  根据起点经纬度和终点经纬度获取距离(米)和时长(分钟)
@@ -20,13 +24,8 @@ public class DirectionService {
      * @return
      */
     public ResponseResult driving(String depLongitude, String depLatitude, String destLongitude, String destLatitude) {
-        log.info("出发地的经度" + depLongitude);
-        log.info("出发地的纬度" + depLatitude);
-        log.info("出发地的经度" + destLongitude);
-        log.info("出发地的纬度度" + destLatitude);
-        DirectionResponse directionResponse = new DirectionResponse();
-        directionResponse.setDistance(123);
-        directionResponse.setDuration(11);
-        return ResponseResult.success(directionResponse);
+        //地图接口的调用
+        DirectionResponse direction = mapDirectionClient.direction(depLongitude, depLatitude, destLongitude, destLatitude);
+        return ResponseResult.success(direction);
     }
 }
