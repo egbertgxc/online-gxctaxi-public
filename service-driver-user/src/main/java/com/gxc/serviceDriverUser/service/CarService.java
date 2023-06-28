e7866696c3267c58659e530195aaa8f1abff00a3
“xegbertgxc <1415051501@qq.com>
2023-06-28 09:20:29 +0800

    计划变更,剩余内容就先不敲代码了

--- service-driver-user/src/main/java/com/gxc/serviceDriverUser/service/CarService.java
+++ service-driver-user/src/main/java/com/gxc/serviceDriverUser/service/CarService.java
@@ -30,20 +30,20 @@ public class CarService {
         // 保存车辆
         carMapper.insert(car);
 
-//        // 获得此车辆的终端id：tid
-//        ResponseResult<TerminalResponse> responseResult = serviceMapClient.addTerminal(car.getVehicleNo(), car.getId()+"");
-//        String tid = responseResult.getData().getTid();
-//        car.setTid(tid);
-//
-//        // 获得此车辆的轨迹id：trid
-//        ResponseResult<TrackResponse> trackResponseResponseResult = serviceMapClient.addTrack(tid);
-//        String trid = trackResponseResponseResult.getData().getTrid();
-//        String trname = trackResponseResponseResult.getData().getTrname();
-//
-//        car.setTrid(trid);
-//        car.setTrname(trname);
-//
-//        carMapper.updateById(car);
+        // 获得此车辆的终端id：tid
+        ResponseResult<TerminalResponse> responseResult = serviceMapClient.addTerminal(car.getVehicleNo(), car.getId()+"");
+        String tid = responseResult.getData().getTid();
+        car.setTid(tid);
+
+        // 获得此车辆的轨迹id：trid
+        ResponseResult<TrackResponse> trackResponseResponseResult = serviceMapClient.addTrack(tid);
+        String trid = trackResponseResponseResult.getData().getTrid();
+        String trname = trackResponseResponseResult.getData().getTrname();
+
+        car.setTrid(trid);
+        car.setTrname(trname);
+
+        carMapper.updateById(car);
 
 
         return ResponseResult.success("");
